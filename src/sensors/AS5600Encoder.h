7e2afa92e8c37d371875571f5f07726da1e3a879
mizhellee <mimizheng2006@gmail.com>
2026-09-11 22:15:40 -0700

    Add AS5600 encoder class scaffold

---
+++ src/sensors/AS5600Encoder.h
@@ -0,0 +1,6 @@
+#pragma once
+
+// Lesson 05: an empty design placeholder, not a working sensor driver.
+class AS5600Encoder {
+    // TODO: use your I2cMaster and distinguish readings from failures.
+};
